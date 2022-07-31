5d57593056ad726e748c340f4f816177a0ed10a1
Paula Letícia Gerónimo <paulagero182@gmail.com>
2022-07-31 09:48:39 -0700

    Chapter 4

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <todo-list :todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from '@/components/TodoList.vue'
 
+const appData = {
+  todos: []
+}
+  
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    TodoList
+  },
+  data() {
+    return appData;
+  },
+  mounted: function() {
+    this.getTodos();
+  },
+  methods: {
+    getTodos: getTodos
   }
+}
+
+async function getTodos() {
+    try {
+      const response = await fetch('api/todos')
+      const data = await response.json()
+      appData.todos = data.list
+    } 
+    catch (error) {
+      console.error(error)
+    }
 }
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
 }
 </style>
